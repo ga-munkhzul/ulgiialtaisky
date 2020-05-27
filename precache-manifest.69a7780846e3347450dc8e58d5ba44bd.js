4538818ba0a80374e6bdaf766ef4bb1321510783
munkhzul <ga.munkhzul@gmail.com>
2020-05-27 17:01:16 +0800

    Updates

---
+++ precache-manifest.69a7780846e3347450dc8e58d5ba44bd.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10f7b9aa2171638c26ed66238a5fa361",
+    "url": "/ulgiialtaisky/index.html"
+  },
+  {
+    "revision": "7f8c2d825428398af36a",
+    "url": "/ulgiialtaisky/static/css/2.403ab2a7.chunk.css"
+  },
+  {
+    "revision": "48ead445030869ae4e3b",
+    "url": "/ulgiialtaisky/static/css/main.0922cce4.chunk.css"
+  },
+  {
+    "revision": "7f8c2d825428398af36a",
+    "url": "/ulgiialtaisky/static/js/2.534dff88.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ulgiialtaisky/static/js/2.534dff88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ead445030869ae4e3b",
+    "url": "/ulgiialtaisky/static/js/main.5144314a.chunk.js"
+  },
+  {
+    "revision": "3174346a01e5b57531ae",
+    "url": "/ulgiialtaisky/static/js/runtime-main.1262e4c9.js"
+  },
+  {
+    "revision": "088c91c8c551efff44b21c21dccbb726",
+    "url": "/ulgiialtaisky/static/media/01-greece.088c91c8.jpg"
+  },
+  {
+    "revision": "8ffe8e3e8b3879a6ec04db7aa3b5fdb9",
+    "url": "/ulgiialtaisky/static/media/02-rome.8ffe8e3e.jpg"
+  },
+  {
+    "revision": "360bb4f45f392b671d40c60909c5618d",
+    "url": "/ulgiialtaisky/static/media/03-japan.360bb4f4.jpg"
+  },
+  {
+    "revision": "05c816131b09a972f1765579345f9a1b",
+    "url": "/ulgiialtaisky/static/media/04-dubai.05c81613.jpg"
+  },
+  {
+    "revision": "0830e9688908e5f3c676a27adea7df14",
+    "url": "/ulgiialtaisky/static/media/05-london.0830e968.jpg"
+  },
+  {
+    "revision": "428c1b4c6690066eca6c0ab40d4f47b3",
+    "url": "/ulgiialtaisky/static/media/06-australia.428c1b4c.jpg"
+  },
+  {
+    "revision": "0c3d42f0528aba238af69d7aeec152a9",
+    "url": "/ulgiialtaisky/static/media/Flaticon.0c3d42f0.woff"
+  },
+  {
+    "revision": "139b8c27fc52634a39f66184f7aae68d",
+    "url": "/ulgiialtaisky/static/media/Flaticon.139b8c27.svg"
+  },
+  {
+    "revision": "24ba5fd427020f5a3a177b91cf9001ac",
+    "url": "/ulgiialtaisky/static/media/Flaticon.24ba5fd4.eot"
+  },
+  {
+    "revision": "6747bea615cb848f002e030a27a4705e",
+    "url": "/ulgiialtaisky/static/media/Flaticon.6747bea6.ttf"
+  },
+  {
+    "revision": "1df593cbc416f3220b9f6452e8f4f61e",
+    "url": "/ulgiialtaisky/static/media/hero_bg_1.1df593cb.jpg"
+  },
+  {
+    "revision": "1dd447b86c348deefc22af04dd9da53a",
+    "url": "/ulgiialtaisky/static/media/hero_bg_2.1dd447b8.jpg"
+  },
+  {
+    "revision": "2cb8dc8186e8827cca3b6beebe4ef1c6",
+    "url": "/ulgiialtaisky/static/media/icomoon.2cb8dc81.eot"
+  },
+  {
+    "revision": "302fbc0aa5dcec40d385772d64baad2c",
+    "url": "/ulgiialtaisky/static/media/icomoon.302fbc0a.svg"
+  },
+  {
+    "revision": "62f33c03d096cdc058166bd6fbf7e4d0",
+    "url": "/ulgiialtaisky/static/media/icomoon.62f33c03.woff"
+  },
+  {
+    "revision": "bab8cf81a530d89b36f24a80649c5e25",
+    "url": "/ulgiialtaisky/static/media/icomoon.bab8cf81.ttf"
+  },
+  {
+    "revision": "19f6adfae8c3b5fae9045e5d3c426b0f",
+    "url": "/ulgiialtaisky/static/media/img_1.19f6adfa.jpg"
+  },
+  {
+    "revision": "1674ebeb971a13b9eb31e54090003182",
+    "url": "/ulgiialtaisky/static/media/img_2.1674ebeb.jpg"
+  },
+  {
+    "revision": "fdc4b4f64d906867e945ab74c52ced59",
+    "url": "/ulgiialtaisky/static/media/img_3.fdc4b4f6.jpg"
+  },
+  {
+    "revision": "7df292240c7204cff11f1e4aa1af9055",
+    "url": "/ulgiialtaisky/static/media/img_4.7df29224.jpg"
+  },
+  {
+    "revision": "bd4db578f943c13e5d067b41a0f2fed6",
+    "url": "/ulgiialtaisky/static/media/person_1.bd4db578.jpg"
+  },
+  {
+    "revision": "68eb6028b7a9b382d52aeffcc8de0dc9",
+    "url": "/ulgiialtaisky/static/media/person_2.68eb6028.jpg"
+  },
+  {
+    "revision": "fe718d55d082327bdc0af2131a9d7dd0",
+    "url": "/ulgiialtaisky/static/media/person_4.fe718d55.jpg"
+  },
+  {
+    "revision": "693b2afb1b496be8d2dd2a5f937fd262",
+    "url": "/ulgiialtaisky/static/media/river.693b2afb.jpeg"
+  },
+  {
+    "revision": "723163dab27b3a96973cacc24dd36dfd",
+    "url": "/ulgiialtaisky/static/media/scene-1.723163da.jpeg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/ulgiialtaisky/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/ulgiialtaisky/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/ulgiialtaisky/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/ulgiialtaisky/static/media/slick.f97e3bbf.svg"
+  }
+]);
